8d9188b69299204d1adf58ea87e6f17a24204b7b
Anna Kalenik <annakalenikalex@gmail.com>
2022-11-02 18:08:57 +0300

    create project structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container header__container'>
+        <a href='/#' className='logo'>MyApp</a>
+      </div>
+    </header>
+  )
+}
+export default Header;
