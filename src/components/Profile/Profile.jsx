afc6b2357920862e37d4174970bd3485f753b3e6
Anna Kalenik <annakalenikalex@gmail.com>
2022-11-03 22:59:47 +0300

    change alt for images in Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,11 +7,11 @@ const Profile = () => {
     <main className={s.main}>
 
       <div className={s.bg}>
-        <img className={s.img} src='https://celes.club/uploads/posts/2022-05/thumbs/1653682560_8-celes-club-p-fon-dlya-uroka-v-zum-krasivie-9.png' alt='Фоновое изображение профиля'></img>
+        <img className={s.img} src='https://celes.club/uploads/posts/2022-05/thumbs/1653682560_8-celes-club-p-fon-dlya-uroka-v-zum-krasivie-9.png' alt='Profile background'></img>
       </div>
       <div className={s.ava}>
         <div className={s.photo}>
-          <img className={s.user_photo} src='https://n1s1.hsmedia.ru/1e/e2/3a/1ee23a077365f02b501d0c815126785c/728x546_1_7dd3ae90748f9a461d8e98347f765534@1616x1212_0xac120003_18985384621638440665.jpeg' alt='Фото пользователя' />
+          <img className={s.user_photo} src='https://n1s1.hsmedia.ru/1e/e2/3a/1ee23a077365f02b501d0c815126785c/728x546_1_7dd3ae90748f9a461d8e98347f765534@1616x1212_0xac120003_18985384621638440665.jpeg' alt="Page owner's photography" />
         </div>
         <div className={s.description}>
           <h2 className={s.name}>Ann Friedman</h2>
