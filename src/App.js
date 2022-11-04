4cabc55d23604366e696784e0f60f87064712093
Anna Kalenik <annakalenikalex@gmail.com>
2022-11-05 01:08:02 +0300

    add routing to navigation and class "activ" for active link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
+import Music from './components/Music/Music';
+import News from './components/News/News';
 import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
 import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Sidebar />
-      <main className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile /> */}
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Sidebar />
+        <main className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/messages' element={<Dialogs />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Sidebar.module.css';
 
 const Sidebar = () => {
   return (
     <aside className={s.sidebar}>
       <nav className={s.nav}>
-        <ul className={s.list}>
-          <li className={s.item}><a href='/#' className={s.link}>Profile</a></li>
-          <li className={s.item}><a href='/#' className={s.link}>Messages</a></li>
-          <li className={s.item}><a href='/#' className={s.link}>News</a></li>
-          <li className={s.item}><a href='/#' className={s.link}>Music</a></li>
-          <li className={s.item}><a href='/#' className={s.link}>Settings</a></li>
+        <ul>
+          <li className={s.item}>
+            <NavLink to='/profile' className={ activeLink => activeLink.isActive ? s.active : s.link }>Profile</NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink to='/messages' className={ activeLink => activeLink.isActive ? s.active : s.link }>Messages</NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink to='/news' className={ activeLink => activeLink.isActive ? s.active : s.link }>News</NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink to='/music' className={ activeLink => activeLink.isActive ? s.active : s.link }>Music</NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink to='/settings' className={ activeLink => activeLink.isActive ? s.active : s.link }>Settings</NavLink>
+          </li>
         </ul>
       </nav>
     </aside>
