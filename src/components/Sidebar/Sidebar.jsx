9738a94c9275f23673a6452062fcb68416fceb86
Anna Kalenik <annakalenikalex@gmail.com>
2022-11-21 20:21:44 +0300

    change links structure

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,23 +6,21 @@ const Sidebar = () => {
   return (
     <aside className={s.sidebar}>
       <nav className={s.nav}>
-        <ul>
-          <li className={s.item}>
-            <NavLink to='/profile' className={ activeLink => activeLink.isActive ? s.active : s.link }>Profile</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to='/messages' className={ activeLink => activeLink.isActive ? s.active : s.link }>Messages</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to='/news' className={ activeLink => activeLink.isActive ? s.active : s.link }>News</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to='/music' className={ activeLink => activeLink.isActive ? s.active : s.link }>Music</NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink to='/settings' className={ activeLink => activeLink.isActive ? s.active : s.link }>Settings</NavLink>
-          </li>
-        </ul>
+          <NavLink to='/profile' className={ activeLink => activeLink.isActive ? s.active : s.link }>
+            <div className={s.name}>Profile</div>
+          </NavLink>
+          <NavLink to='/messages' className={ activeLink => activeLink.isActive ? s.active : s.link }>
+            <div className={s.name}>Messages</div>
+          </NavLink>
+          <NavLink to='/news' className={ activeLink => activeLink.isActive ? s.active : s.link }>
+            <div className={s.name}>News</div>
+          </NavLink>
+          <NavLink to='/music' className={ activeLink => activeLink.isActive ? s.active : s.link }>
+            <div className={s.name}>Music</div>
+          </NavLink>
+          <NavLink to='/settings' className={ activeLink => activeLink.isActive ? s.active : s.link }>
+            <div className={s.name}>Settings</div>
+          </NavLink>
       </nav>
     </aside>
   )
